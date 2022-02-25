b47138eadda318704d2c14e265a9bdd062127c6b
niyari <niyari@users.noreply.github.com>
2022-02-26 05:30:48 +0900

    Add project files.

---
+++ src/WebView2Sample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Web.WebView2.Core;
+using System;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WebView2Sample
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            myWebView2_InitializeAsync();
+        }
+
+        private async void myWebView2_InitializeAsync()
+        {
+            await myWebView2.EnsureCoreWebView2Async();
+            if (myWebView2.CoreWebView2 != null)
+            {
+                myWebView2.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                    "appassets.example", "AppAssets", CoreWebView2HostResourceAccessKind.DenyCors);
+                myWebView2.Source = new Uri("https://appassets.example/res/html/index.html");
+            }
+        }
+    }
+}
